4548ccfb9a1f37ca1a34ff98ade1d1c69fd6dc7b
Zimeng Chen <eagleofpc@gmail.com>
2015-11-21 23:40:48 -0500

    Add extra line

--- excel_importer/serializer/sql/benefits/hra_selection_serializer.py
+++ excel_importer/serializer/sql/benefits/hra_selection_serializer.py
@@ -4,3 +4,4 @@ class HraSelectionSerializer(object):
         if hra_selection and hra_selection.benefit_plan:
             file.write('  INSERT INTO app_personcompanyhraplan(created_at, updated_at, company_hra_plan_id, person_id)\n')
             file.write('  VALUES(now(), now(), {}, {});\n'.format(hra_selection.benefit_plan.id, person_id_string))
+            file.write('\n')
